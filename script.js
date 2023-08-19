01cbea749320a0bf113197656f037c64e0aab5cb
JD Nafziger <nafziger10@gmail.com>
2023-08-19 01:28:20 -0500

    added empty field tests and red boxes

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ $(document).ready(onReady);
 
 function onReady(){
     $("#submit-btn").on('click',submitUser);
+    // allows you to hit enter after typing salary to submit.
+    $("#inSalary").on('keyup', hitEnter);
+
+
     $("#resultsTable").on('click','.deleteTarget',removeRow);
     autoAdd(); //used to AUTO GENERATE USERS 
 }//end onReady
 
-//
+
+
 let employees = {}
-// employee = {'123': employeeObj1}, {'456': employeeObj2}, ...
+// employee = {'123': {}  }, {'456': employeeObj2}, ...
 // access it with employees[123]
+
+function hitEnter(e){
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        $("#submit-btn").trigger("click");
+    }
+}
 
 function submitUser(){
     
@@ -17,8 +28,61 @@ function submitUser(){
     let lastNameInput = $('#inLastName').val();
     let idInput = $('#inUserID').val();
     let titleInput = $('#inTitle').val();
-    let salaryInput = Number($('#inSalary').val());
-    
+    let inSalary = $('#inSalary').val()
+    let salaryInput = Number(inSalary); //separate before making a number to check if string is empty.
+
+    //if id was already entered
+    const iterable = Object.keys(employees);
+    for (let id of iterable){
+        if (idInput === id){
+            alert(`Oops, the id "${id}" has already been used. Please double check the user ID.`)
+            $('#inUserID').val('');
+            $('#inUserID').addClass('error-highlight');
+            return;
+        } else {
+            $('#inUserID').removeClass('error-highlight');
+        }
+    }
+
+    //if fields are empty
+    if (firstNameInput === "" || lastNameInput === "" || titleInput === "" || idInput === ""){
+        if (firstNameInput === "" || inSalary === "") {
+            $('#inFirstName').addClass('error-highlight');
+            } else {
+                $('#inFirstName').removeClass('error-highlight');
+            }
+        if (lastNameInput === "" ) {
+            $('#inLastName').addClass('error-highlight');
+            } else {
+                $('#inLastName').removeClass('error-highlight');
+            }
+        if (titleInput === ""){
+                $('#inTitle').addClass('error-highlight');
+            } else {
+                $('#inTitle').removeClass('error-highlight');
+            }
+        if (idInput === ""){
+                $('#inUserID').addClass('error-highlight');
+            } else {
+                $('#inUserID').removeClass('error-highlight');
+            }
+        if (inSalary === ""){
+                $('#inSalary').addClass('error-highlight');
+            } else {
+                $('#inSalary').removeClass('error-highlight');
+            }
+        alert("Please fill in all fields to submit.")
+        return;
+    }//end if empties
+
+    // catch if the last one corrected before submit.
+    $('#inFirstName').removeClass('error-highlight');
+    $('#inLastName').removeClass('error-highlight');
+    $('#inTitle').removeClass('error-highlight');
+    $('#inSalary').removeClass('error-highlight');
+    $('#idInput').removeClass('error-highlight');
+
+
     // layout of the employee object:
     let employee = { 
         firstName: firstNameInput,
@@ -29,7 +93,7 @@ function submitUser(){
     }
     
     //add to total list of employees
-    employees[idInput]= employee;
+    employees[idInput]= employee; 
 
     addRow(employee);
     //add data to table
@@ -42,6 +106,8 @@ function submitUser(){
     $('#inSalary').val('');
 
 }// end submitUser
+
+
 
 function addRow(employee){    
     //format the string to append
@@ -79,6 +145,7 @@ function updateMonthly(employee,updown){
 }//end updateTotal
 
 function removeRow(){
+    console.log($(this).parent().parent()[0]);
     let htmlTarget = $(this).parent().parent()[0]; 
     let employeeId = htmlTarget.id
     $(this).parent().parent().remove()
@@ -88,7 +155,7 @@ function removeRow(){
 
 //Format number into currency display
 function formatMoney(number){
-    let formatting_options = { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }
+    let formatting_options = { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }
     
     let salaryFormatted = number.toLocaleString("en-US",formatting_options);
     return salaryFormatted
